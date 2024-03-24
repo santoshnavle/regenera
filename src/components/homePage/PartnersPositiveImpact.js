90c79d24140ec82fe7f760a98b592614982b25ea
santosh-mdlz <santosh.navle@mdlz.com>
2024-03-24 10:50:03 +0530

    undo changes

--- src/components/homePage/PartnersPositiveImpact.js
+++ src/components/homePage/PartnersPositiveImpact.js
@@ -228,7 +228,7 @@ const PartnersImpact = () => {
                                     
                                 }
                                 .card-button-mobile{
-                                    display: none;
+                                    display: block;
                                 }
                             }
                         }
